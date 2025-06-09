27d0d22d049ea51c99566fc7eaae4fea2f72f8c4
MrSeager <seager1993@gmail.com>
2025-06-09 11:40:19 +0300

    6/9/2025

--- src/components/SectorOne.tsx
+++ src/components/SectorOne.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react';
 //Bootstrap
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button, Image, Carousel } from 'react-bootstrap';
 //Images
-import DesktopImg from '../images/free-photo-of-minimalist-setup-office.jpeg';
+import ImgOne from '../images/free-photo-of-minimalist-setup-office.jpeg';
+import ImgTwo from '../images/pexels-ben-collins-41034267-7292641.jpg';
+import ImgThree from '../images/pexels-drew-williams-1285451-3143813.jpg';
+import ImgFour from '../images/pexels-knownasovan-57690.jpg';
 
 const SectorOne: FC = () => {
     return (
@@ -23,8 +26,21 @@ const SectorOne: FC = () => {
                         Get your free account
                     </Button>
                 </Col>
-                <Col lg={{ span: 6, order: 2 }} xs={{ span: 12, order: 1 }} className='p-4'>
-                    <Image fluid src={DesktopImg} alt='image' className='rounded rounded-3' />
+                <Col lg={{ span: 6, order: 2 }} xs={{ span: 12, order: 1 }} className='p-4 d-flex flex-column align-items-center justify-content-center'>
+                    <Carousel interval={2000} controls={false} indicators={false}>
+                        <Carousel.Item>
+                            <Image fluid src={ImgOne} alt='image' className='rounded rounded-3' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgTwo} alt='image' className='rounded rounded-3' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgThree} alt='image' className='rounded rounded-3' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgFour} alt='image' className='rounded rounded-3' />
+                        </Carousel.Item>
+                    </Carousel>
                 </Col>
             </Row>
         </Container>
--- src/components/SectorThree.tsx
+++ src/components/SectorThree.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 //Bootstrap
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button, Image, Carousel } from 'react-bootstrap';
 //Icons
 import { FaArrowRight } from "react-icons/fa6";
 import { GoStarFill } from "react-icons/go";
 //Images
-import Img from '../images/pexels-teono123-no-18036-88476.jpg';
-import ImgTwo from '../images/pexels-designecologist-1779487.jpg';
+import ImgFirstOne from '../images/pexels-pixabay-207691.jpg';
+import ImgFirstTwo from '../images/pexels-polina-tankilevitch-4443160.jpg';
+import ImgFirstThree from '../images/pexels-rdne-7092350.jpg';
+
+import ImgSecondOne from '../images/pexels-teono123-no-18036-88476.jpg';
+import ImgSecondTwo from '../images/pexels-designecologist-1779487.jpg';
+import ImgSecondThree from '../images/pexels-pixabay-267350.jpg';
 
 const SectorThree: FC = () => {
     return (
@@ -27,7 +32,17 @@ const SectorThree: FC = () => {
                     <Button className='cs-transition bg-transparent border-0 cs-tc-two fw-bold px-0 cs-link'>Learn more about courses <FaArrowRight /></Button>
                 </Col>
                 <Col lg={{ span: 6, order: 3 }} xs={{ span: 12, order: 2 }} className='py-3 mx-auto'>
-                    <Image fluid src={Img} alt='image' className='rounded rounded-3 border border-2' />
+                    <Carousel interval={2000} controls={false} indicators={false}>
+                        <Carousel.Item>
+                            <Image fluid src={ImgFirstOne} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgFirstTwo} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgFirstThree} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                    </Carousel>
                 </Col>
                 <Col xs={{ span: 12, order: 4 }} className='d-flex flex-column align-items-center gap-3 py-5 text-center'>
                     <Container className='d-flex flex-row gap-3 justify-content-center h3 text-warning'>
@@ -51,7 +66,17 @@ const SectorThree: FC = () => {
                     <Button className='cs-transition bg-transparent border-0 cs-tc-two fw-bold px-0 cs-link'>Learn more about downloads <FaArrowRight /></Button>
                 </Col>
                 <Col lg={{ span: 6, order: 5 }} xs={{ span: 12, order: 6 }} className='py-3 mx-auto'>
-                    <Image fluid src={ImgTwo} alt='image' className='rounded rounded-3 border border-2' />
+                    <Carousel interval={2000} controls={false} indicators={false}>
+                        <Carousel.Item>
+                            <Image fluid src={ImgSecondOne} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgSecondTwo} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image fluid src={ImgSecondThree} alt='image' className='cs-carousel-image rounded rounded-3 border border-2' />
+                        </Carousel.Item>
+                    </Carousel>
                 </Col>
                 <Col lg={{ order: 5 }} xs={{ span: 12, order: 7 }} className='d-flex flex-column align-items-center gap-3 py-5 text-center'>
                     <Container className='d-flex flex-row gap-3 justify-content-center h3 text-warning'>
